c72d8fb37f30463891ea5601941a941ebd0a99c1
Tom Kidumbuyo <thomas.kidumbuyo@politicaldata.com>
2023-01-24 00:04:20 +0300

    moved orm config

---
+++ src/database/config/ormconfig.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(`${__dirname}/**/*.entity.js`);
+
+const ormConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 8889,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [
+    `${__dirname}/**/*.entity.js`,
+    'node_modules/nestjs-admin/**/*.entity.js',
+  ],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
+  migrationsTableName: 'migrations',
+  synchronize: false,
+};
+
+export default ormConfig;

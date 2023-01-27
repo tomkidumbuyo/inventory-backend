58a00d03d46afcd4eb61a3fbb3a153a79fc9634c
Tom Kidumbuyo <thomas.kidumbuyo@politicaldata.com>
2023-01-28 02:41:11 +0300

    Add variable to user entity

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -1,10 +1,31 @@
 import { Column, CreateDateColumn, Entity, UpdateDateColumn } from 'typeorm';
 import BaseEntity from './base.entity';
 
+export enum UserTypeEnum {
+  ISSUER = 'ISSUER',
+  STORE_KEEPER = 'STORE_KEEPER',
+  APPROVER = 'APPROVER',
+}
+
 @Entity('users')
 export default class UserEntity extends BaseEntity {
   @Column({ name: 'email' })
   email: string;
+
+  @Column({ name: 'first_name' })
+  firstName: string;
+
+  @Column({ name: 'last_name' })
+  lastName: string;
+
+  @Column({ name: 'phone_number' })
+  phoneNumber: string;
+
+  @Column({ name: 'user_type' })
+  userType: UserTypeEnum;
+
+  @Column({ name: 'password' })
+  password: string;
 
   @CreateDateColumn({ name: 'created_at', nullable: false })
   createdAt: Date;

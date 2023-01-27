dd7e04d09c3a56d8259c956ef4f7496bfb9d58fe
Tom Kidumbuyo <thomas.kidumbuyo@politicaldata.com>
2023-01-28 01:30:54 +0300

    Add swagger in the project
    I added nest js swagger to the project.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Inventory Management System')
+    .setDescription(
+      'This is  a simple system to manage inventory for amplify marketing group',
+    )
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
   await app.listen(parseInt(process.env.BACKEND_PORT) || 3030);
 }
 bootstrap();
